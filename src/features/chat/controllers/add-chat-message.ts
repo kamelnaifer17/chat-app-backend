7f5ec5e649d71ab862e15ddc2833b86cd7e46f4c
kamelnaifer17 <kamelnaifer17>
2023-10-22 22:57:04 +0100

    first commit

---
+++ src/features/chat/controllers/add-chat-message.ts
@@ -0,0 +1,95 @@
+import { Request, Response } from 'express';
+import HTTP_STATUS from 'http-status-codes';
+import { UserCache } from '@service/redis/user.cache';
+import { IUserDocument } from '@user/interfaces/user.interface';
+import { joiValidation } from '@global/decorators/joi-validation.decorators';
+import { addChatSchema } from '@chat/schemes/chat';
+import { ObjectId } from 'mongodb';
+import mongoose from 'mongoose';
+import { UploadApiResponse } from 'cloudinary';
+import { uploads } from '@global/helpers/cloudinary-upload';
+import { BadRequestError } from '@global/helpers/error-handler';
+import { IMessageData, IMessageNotification } from '@chat/interfaces/chat.interface';
+import { socketIOChatObject } from '@socket/chat';
+import { MessageCache } from '@service/redis/message.cache';
+import { chatQueue } from '@service/queues/chat.queue';
+
+const userCache: UserCache = new UserCache();
+const messageCache: MessageCache = new MessageCache();
+
+export class Add {
+  @joiValidation(addChatSchema)
+  public async message(req: Request, res: Response): Promise<void> {
+    const {
+      conversationId,
+      receiverId,
+      receiverUsername,
+      receiverAvatarColor,
+      receiverProfilePicture,
+      body,
+      gifUrl,
+      isRead,
+      selectedImage
+    } = req.body;
+    let fileUrl = '';
+    const messageObjectId: ObjectId = new ObjectId();
+    const conversationObjectId: ObjectId = !conversationId ? new ObjectId() : new mongoose.Types.ObjectId(conversationId);
+
+    const sender: IUserDocument = await userCache.getUserFromCache(`${req.currentUser!.userId}`) as IUserDocument;
+
+    if (selectedImage.length) {
+      const result: UploadApiResponse = (await uploads(req.body.image, req.currentUser!.userId, true, true)) as UploadApiResponse;
+      if (!result?.public_id) {
+        throw new BadRequestError(result.message);
+      }
+      fileUrl = `https://res.cloudinary.com/dyamr9ym3/image/upload/v${result.version}/${result.public_id}`;
+    }
+
+    const messageData: IMessageData = {
+      _id: `${messageObjectId}`,
+      conversationId: new mongoose.Types.ObjectId(conversationObjectId),
+      receiverId,
+      receiverAvatarColor,
+      receiverProfilePicture,
+      receiverUsername,
+      senderUsername: `${req.currentUser!.username}`,
+      senderId: `${req.currentUser!.userId}`,
+      senderAvatarColor: `${req.currentUser!.avatarColor}`,
+      senderProfilePicture: `${sender.profilePicture}`,
+      body,
+      isRead,
+      gifUrl,
+      selectedImage: fileUrl,
+      createdAt: new Date(),
+      deleteForEveryone: false,
+      deleteForMe: false
+    };
+    Add.prototype.emitSocketIOEvent(messageData);
+
+
+    await messageCache.addChatListToCache(`${req.currentUser!.userId}`, `${receiverId}`, `${conversationObjectId}`);
+    await messageCache.addChatListToCache(`${receiverId}`, `${req.currentUser!.userId}`, `${conversationObjectId}`);
+    await messageCache.addChatMessageToCache(`${conversationObjectId}`, messageData);
+    chatQueue.addChatJob('addChatMessageToDB', messageData);
+
+    res.status(HTTP_STATUS.OK).json({ message: 'Message added', conversationId: conversationObjectId });
+  }
+
+  public async addChatUsers(req: Request, res: Response): Promise<void> {
+    const chatUsers = await messageCache.addChatUsersToCache(req.body);
+    socketIOChatObject.emit('add chat users', chatUsers);
+    res.status(HTTP_STATUS.OK).json({ message: 'Users added'});
+  }
+
+  public async removeChatUsers(req: Request, res: Response): Promise<void> {
+    const chatUsers = await messageCache.removeChatUsersFromCache(req.body);
+    socketIOChatObject.emit('add chat users', chatUsers);
+    res.status(HTTP_STATUS.OK).json({ message: 'Users removed'});
+  }
+
+  private emitSocketIOEvent(data: IMessageData): void {
+    socketIOChatObject.emit('message received', data);
+    socketIOChatObject.emit('chat list', data);
+  }
+
+}
